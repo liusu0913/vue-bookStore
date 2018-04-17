7a6c390fdb8a58b48e15b0f248d716bf2faee909
柴米油盐 <471097828@qq.com>
2018-04-17 11:01:40 +0800

    liusu0913

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Home from '../Home.vue'
 import Cart from '../Cart.vue'
 import Me from '../Me.vue'
